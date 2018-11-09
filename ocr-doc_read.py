08b6683b49d3d06cd091ad9cf726c5784b4931b5
Methunas <40215905+Methunas@users.noreply.github.com>
2018-11-09 11:48:01 -0500

    Character confidence
    Return value of the document detection is now an array of words containing pairs

--- ocr-doc_read.py
+++ ocr-doc_read.py
@@ -4,8 +4,14 @@ import os
 
 from google.cloud import vision
 
+
 def detect_document(filein):
-    """Detects document features in an image."""
+    """
+    Detects document features in an image.
+    Image file passed in must be a MRZ.
+    Returns an array of words containing paired strings/doubles.
+    eg. paragraph[word][char position][char, confidence]
+    """
     client = vision.ImageAnnotatorClient()
 
     with io.open(filein, 'rb') as image_file:
@@ -15,50 +21,57 @@ def detect_document(filein):
 
     response = client.document_text_detection(image=image)
 
-    parse_string = ''
-    parsed_text = []
-
     for page in response.full_text_annotation.pages:
         for block in page.blocks:
-            # print('\nBlock confidence: {}\n'.format(block.confidence))
-
             for paragraph in block.paragraphs:
-                # print('Paragraph confidence: {}'.format(paragraph.confidence))
+                parsed_par = []
 
                 for word in paragraph.words:
-                    word_text = ''.join([
-                        symbol.text for symbol in word.symbols
-                    ])
-                    print('Word text: {} (confidence: {})'.format(word_text, word.confidence))
+                    parsed_word = []
 
                     for symbol in word.symbols:
-                        # print('\tSymbol: {} (confidence: {})'.format(symbol.text, symbol.confidence))
-                        parse_string += symbol.text
-                    if '<' not in parse_string and '\u00AB' not in parse_string:
-                        parsed_text.append(parse_string)
-                    parse_string = ''
 
-    print(parsed_text)
-    return parsed_text
+                        if symbol.text != '<' and symbol.text != '\u00AB' :
+                            parsed_word.append([symbol.text, symbol.confidence])
+
+                    if parsed_word:
+                        parsed_par.append(parsed_word)
+
+    print(parsed_par)
+    return parsed_par
 
 
 def to_file(text_array, fileout):
-    """Outputs an array of strings to a file"""
+    """
+    Outputs an array of strings to two files based on content
+    filename_text.txt contains text parsed from document, each word on a different line
+    filename_confidence.txt contains the confidence for each word, character by character
+    """
     if fileout == 0:
-        filename = 'result.txt'
+        filename_text = 'result_text.txt'
+        filename_conf = 'result_confidence.txt'
     else:
-        filename = fileout
+        filename_text = fileout + '_text.txt'
+        filename_conf = fileout + '_confidence.txt'
 
-    file = open(filename, 'w+')
+    file_text = open(filename_text, 'w+')
+    file_conf = open(filename_conf, 'w+')
 
     for word in text_array:
-        file.write(word + '\n')
+        for character in word:
+            file_text.write(character[0])
+            file_conf.write(str(character[1]) + ' ')
+        file_text.write('\n')
+        file_conf.write('\n')
 
-    print('Wrote results to \"' + filename + '\"')
-    file.close()
+    print('Wrote results to \"' + filename_text + '\" and \"' + filename_conf + '\"')
+    file_text.close()
+    file_conf.close()
 
-def setKey(path):
+
+def set_key(path):
     os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = path
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -66,8 +79,7 @@ if __name__ == '__main__':
     parser.add_argument('-out_file', help='Optional output file', default=0)
     args = parser.parse_args()
 
-    # Argument should be the path to the API key .json file
-    setKey('ocr.json')
+    set_key('ocr.json')
 
     parser = argparse.ArgumentParser()
     text = detect_document(args.detect_file)
